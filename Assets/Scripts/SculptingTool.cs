5bef8afdf2610353a1634fd09a092d1b1347d1ec
student <student@m506-215-26742.local>
2016-07-15 12:26:41 -0700

    Early Interaction

---
+++ Assets/Scripts/SculptingTool.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SculptingTool : MonoBehaviour {
+
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+		prefab = this.gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(RotateInterface.getState() == RotateInterface.LEFT_CLICK || RotateInterface.getState() == RotateInterface.RIGHT_CLICK)
+		{
+
+			Instantiate(prefab, this.transform.position, Quaternion.identity);
+
+		}
+	
+	}
+}
